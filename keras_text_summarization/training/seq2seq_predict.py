d340ac9d45663cb480ce80d85fa183ba3e5a2791
xschen <xs0040@gmail.com>
2017-12-30 13:00:55 +0800

    predict script for the seq2seq

--- keras_text_summarization/training/seq2seq_predict.py
+++ keras_text_summarization/training/seq2seq_predict.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from keras_text_summarization.utility.plot_utils import plot_and_save_history
 from keras_text_summarization.library.seq2seq import Seq2Seq
 from keras_text_summarization.utility.fake_news_loader import fit_text
 import numpy as np
@@ -11,7 +9,6 @@ import numpy as np
 def main():
     np.random.seed(42)
     data_dir_path = './data'
-    report_dir_path = './reports'
     model_dir_path = './models'
 
     print('loading csv file ...')
@@ -34,18 +31,13 @@ def main():
     print('configuration extracted from input texts ...')
 
     classifier = Seq2Seq(config)
-    # classifier.load_weights(weight_file_path=Seq2Seq.get_weight_file_path(model_dir_path=model_dir_path))
+    classifier.load_weights(weight_file_path=Seq2Seq.get_weight_file_path(model_dir_path=model_dir_path))
 
-    Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2, random_state=42)
+    print('start predicting ...')
+    for x in X[0:20]:
+        headline = classifier.summarize(x)
+        print(headline)
 
-    print('training size: ', len(Xtrain))
-    print('testing size: ', len(Xtest))
-
-    print('start fitting ...')
-    history = classifier.fit(Xtrain, Ytrain, Xtest, Ytest)
-
-    history_plot_file_path = report_dir_path + '/' + Seq2Seq.model_name + '-history.png'
-    plot_and_save_history(history, classifier.model_name, history_plot_file_path)
 
 
 if __name__ == '__main__':
