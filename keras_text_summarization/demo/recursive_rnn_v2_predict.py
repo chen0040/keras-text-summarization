242a2ec1a031ddc20eabe7359c911b11150627dd
xschen <xs0040@gmail.com>
2018-02-07 10:37:49 +0800

    rnn2 predict and train script

---
+++ keras_text_summarization/demo/recursive_rnn_v2_predict.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import pandas as pd
+from keras_text_summarization.library.rnn import RecursiveRNN2
+import numpy as np
+
+
+def main():
+    np.random.seed(42)
+    data_dir_path = './data'
+    model_dir_path = './models'
+
+    print('loading csv file ...')
+    df = pd.read_csv(data_dir_path + "/fake_or_real_news.csv")
+    # df = df.loc[df.index < 1000]
+    X = df['text']
+    Y = df.title
+
+    config = np.load(RecursiveRNN2.get_config_file_path(model_dir_path=model_dir_path)).item()
+
+    summarizer = RecursiveRNN2(config)
+    summarizer.load_weights(weight_file_path=RecursiveRNN2.get_weight_file_path(model_dir_path=model_dir_path))
+
+    print('start predicting ...')
+    for i in np.random.permutation(np.arange(len(X)))[0:20]:
+        x = X[i]
+        actual_headline = Y[i]
+        headline = summarizer.summarize(x)
+        # print('Article: ', x)
+        print('Generated Headline: ', headline)
+        print('Original Headline: ', actual_headline)
+
+
+if __name__ == '__main__':
+    main()
